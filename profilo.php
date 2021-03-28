5d2316d79330e654d42f7656d1bebdd3052cae9e
squirlyfoxy <public.thesquirlyz97@gmail.com>
2021-03-28 22:02:39 +0200

    [SITO] -> Fatto cose

--- profilo.php
+++ profilo.php
@@ -19,12 +19,14 @@
 <section id="profilo" class="about d-flex align-items-center">
     <div class="container" data-aos="zoom-out" data-aos-delay="100"> 
         <div class="section-title">
-          <h2>Profilo</h2> <br>
-          <?php echo $usr["nome"]; echo " "; echo $usr["cognome"];?> <br>
+          <h2>Profilo</h2> <br> <br>
+          <img src="./assets/img/team/team-2.jpg" alt="Avatar" style="border-radius: 50%;" width="200px"> <br> <br>
+          <b> <?php echo $usr["nome"]; echo " "; echo $usr["cognome"];?> </b> <br>
           <hr>
           Codice Fiscale: <?php echo $usr["cf"]; ?> <br>
           Indirizzo: <?php echo $usr["indirizzo"]; ?> <br>
           Numero di Telefono: <?php echo $usr["telefono"]; ?> <br>
+          Indirizzo: <?php echo $usr["citta"]; echo ", "; echo $usr["indirizzo"]; echo " (". $usr["provincia"]. ")"; ?> <br>
         </div>
     </div>
 </section>
