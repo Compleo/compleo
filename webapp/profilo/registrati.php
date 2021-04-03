4feab2f677e7c81fa39201e2a9d5b5dd87792960
squirlyfoxy <public.thesquirlyz97@gmail.com>
2021-04-03 18:43:12 +0200

    [SITO] -> Ini<io frontend per la registrazione

--- webapp/profilo/registrati.php
+++ webapp/profilo/registrati.php
@@ -8,7 +8,7 @@
 
 <html lang="it">
     <head>
-        <title>Compleo - Login</title>
+        <title>Compleo - Registrati</title>
 
         <!-- CSS !-->
         <link rel="stylesheet" type="text/css" href="../assets/semantic/semantic.min.css">
@@ -31,13 +31,59 @@
             <div class="ui middle aligned grid child">
                 <div class="column">
                     <div class="ui center aligned page grid">
-                        <form method="POST" action="../php/registraUtente_verifica.php">
-
-                        </form>
+                        <h2>Seleziona un tipo di account</h2>
+                        <div class="ui cards">
+                                <div class="card">
+                                    <div class="content">
+                                        <div class="header">
+                                            Account Base
+                                        </div>
+                                        <div class="meta">
+                                            Il modo più semplice per accedere ai servizi di Compleo.
+                                        </div>
+                                        <div class="description">
+                                            Con l'account base accedi alla possibilità di trovare lavoratori, da un semplice maggiordomo ad un elettricista passando per il tuo infermiere di fiducia. I costi? la volontà di iscriversi
+                                            <br>
+                                            <br>
+                                            <center>
+                                                <a href="./registrati-base.php">
+                                                    <button class="ui button">
+                                                        Seleziona
+                                                    </button>
+                                                </a>
+                                            </center>
+                                            <br>
+                                            Puoi passare all'account completo in qualsiasi momento.
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="card">
+                                    <div class="content">
+                                        <div class="header">
+                                            Account Completo
+                                        </div>
+                                        <div class="meta">
+                                            Ottieni la possibilità di lavorare con noi.
+                                        </div>
+                                        <div class="description">
+                                            Con l'account Completo ottieni la possibilità di proporti come lavoratore senza perdere i benefici dell'account Base. Compleo è il modo migliore per iniziare a fare piccoli lavoretti in modo del tutto legale e veloce; Inizia subito!
+                                            <br>
+                                            <br>
+                                            <br>
+                                            <center>
+                                                <a href="./registrati-completo.php">
+                                                    <button class="ui button">
+                                                        Seleziona
+                                                    </button>
+                                                </a>
+                                            </center>
+                                        </div>
+                                    </div>
+                                </div>
+                        </div>
                     </div>
                 </div>
             </div>
-
 
         <!-- JS !-->
         <script
